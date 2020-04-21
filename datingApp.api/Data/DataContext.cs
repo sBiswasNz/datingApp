b928bd8d24dab1e701dbc71864c867a6dbf298e2
oneCan <s.biswas_nz@outlook.com>
2020-04-22 11:51:31 +1200

    Initial Commit

---
+++ datingApp.api/Data/DataContext.cs
@@ -0,0 +1,14 @@
+using datingApp.api.Models;
+using Microsoft.EntityFrameworkCore; 
+
+namespace datingApp.api.Data
+
+{
+    public class DataContext : DbContext 
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options){}
+
+        public DbSet<Value> Values { get; set; }
+        
+    }
+}
